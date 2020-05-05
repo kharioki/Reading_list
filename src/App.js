01e7219dcd2857d0b6209ebdae36a7eb15565390
kharioki <antolisah@gmail.com>
2020-05-05 11:25:41 +0300

    theme context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import ThemeContextProvider from './contexts/ThemeContext';
+
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
