da6bb7ebf87895096703200d4b7651285c486742
kharioki <antolisah@gmail.com>
2020-05-05 14:01:15 +0300

    nesting multiple context providers

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import ThemeContextProvider from './contexts/ThemeContext';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
