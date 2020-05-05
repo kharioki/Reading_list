2d82d5b3eb98500bf830a39f062d30b899c9f9f8
kharioki <antolisah@gmail.com>
2020-05-05 11:53:08 +0300

    accessing context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 function BookList() {
+  const contextType = useContext(ThemeContext);
+
+  const { isLightTheme, light, dark } = contextType;
+  const theme = isLightTheme ? light : dark;
   return (
-    <div className="book-list">
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
       <ul>
-        <li>the way of kings</li>
-        <li>the name of the wind</li>
-        <li>the final empire</li>
+        <li style={{ background: theme.ui }}>the way of kings</li>
+        <li style={{ background: theme.ui }}>the name of the wind</li>
+        <li style={{ background: theme.ui }}>the final empire</li>
       </ul>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 function Navbar() {
+  const contextType = useContext(ThemeContext);
+
+  const { isLightTheme, light, dark } = contextType;
+  const theme = isLightTheme ? light : dark;
   return (
-    <div>
-      <nav>
-        <h1>Reading list</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </div>
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Reading list</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   );
 }
 
