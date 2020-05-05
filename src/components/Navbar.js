04abc9ab132938debb81f0a09581cd2d2e4d0bc2
kharioki <antolisah@gmail.com>
2020-05-05 12:24:17 +0300

    using consumers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,26 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 function Navbar() {
-  const contextType = useContext(ThemeContext);
+  // const contextType = useContext(ThemeContext);
 
-  const { isLightTheme, light, dark } = contextType;
-  const theme = isLightTheme ? light : dark;
   return (
-    <nav style={{ background: theme.ui, color: theme.syntax }}>
-      <h1>Reading list</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <ThemeContext.Consumer>
+      {context => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return (
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Reading list</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        );
+      }}
+    </ThemeContext.Consumer>
   );
 }
 
