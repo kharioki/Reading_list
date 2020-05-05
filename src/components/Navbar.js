886f43cbe0928f06a23c5ac002331f74a3c0c601
kharioki <antolisah@gmail.com>
2020-05-05 14:31:37 +0300

    using multiple contexts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 function Navbar() {
-  // const contextType = useContext(ThemeContext);
-
   return (
-    <ThemeContext.Consumer>
-      {context => {
-        const { isLightTheme, light, dark } = context;
-        const theme = isLightTheme ? light : dark;
-        return (
-          <nav style={{ background: theme.ui, color: theme.syntax }}>
-            <h1>Reading list</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        );
-      }}
-    </ThemeContext.Consumer>
+    <AuthContext.Consumer>
+      {({ isAuthenticated, toggleAuth }) => (
+        <ThemeContext.Consumer>
+          {themeContext => {
+            const { isLightTheme, light, dark } = themeContext;
+            const theme = isLightTheme ? light : dark;
+            return (
+              <nav style={{ background: theme.ui, color: theme.syntax }}>
+                <h1>Reading list</h1>
+                <div onClick={toggleAuth}>
+                  {isAuthenticated ? 'Logged In' : 'Logged Out'}
+                </div>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            );
+          }}
+        </ThemeContext.Consumer>
+      )}
+    </AuthContext.Consumer>
   );
 }
 
